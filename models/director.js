358190ffe3e92b80abe08fed5a046dc0bc390bc5
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-19 14:23:46 +0530

    Correct the file

--- models/director.js
+++ models/director.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const directorSchema = new mongoose.Schema({
     name : { type: String, required: true },
-    email : { type: string, required: true },
+    email : { type: String, required: true },
     country : { type: String, required: true },
     created_films : Number,
 })
