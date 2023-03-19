79517264c48fec84d7fda6d1e1f03c566b197c80
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-19 09:56:50 +0530

    Create movie.js file

---
+++ routes/movies.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+//Getting all movies
+router.get('/', (req, res) => {
+
+})
+
+//Getting one movie
+router.get('/:id', (req, res) => {
+
+})
+
+//Creating a movie
+router.post('/', async (req, res) => {
+
+})
+
+//Updating a movie
+router.patch('/:id', async (req, res) => {
+
+})
+
+//Deleting a movie
+router.delete('/:id', async (req, res) => {
+
+})
+
+module.exports = router
