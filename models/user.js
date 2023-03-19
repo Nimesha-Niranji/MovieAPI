42ecb10354f1a5cd6d100fc0573413f3d96f28ae
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-19 13:58:07 +0530

    Edit

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: string, required: true },
+    email: { type: String, required: true },
     country : { type: String, required: true },
     telephone: Number,
 })
