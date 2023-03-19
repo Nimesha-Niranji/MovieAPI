80509c70f26f3a5082054dcb4267a0453b4d7734
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-03-19 10:47:03 +0530

    Create post and get movie functions

--- routes/movies.js
+++ routes/movies.js
@@ -19,7 +19,17 @@ router.get('/:id', (req, res) => {
 
 //Creating a movie
 router.post('/', async (req, res) => {
-
+    const movie = new Movie({
+        title: req.body.title,
+        year: req.body.year,
+        director: req.body.director
+      })
+      try {
+        const newMovie = await movie.save()
+        res.status(201).json(newMovie)
+      } catch (err) {
+        res.status(400).json({ message: err.message })
+      }
 })
 
 //Updating a movie
@@ -31,5 +41,20 @@ router.patch('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
 
 })
+
+async function getmovie(req, res, next) {
+    let movie
+    try {
+      movie = await Movie.findById(req.params.id)
+      if (movie == null) {
+        return res.status(404).json({ message: 'Cannot find subscriber' })
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message })
+    }
+  
+    res.movie = movie
+    next()
+  }
 
 module.exports = router
